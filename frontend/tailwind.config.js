91fd7519a468eebd68e18694f906dee94de34586
Henrique Chaves <henriquelopes0611@gmail.com>
2024-06-11 09:05:38 -0300

    initial setup plus mobile header design

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      display: ["Nunito", "sans-serif"],
+    },
+  },
+  plugins: [],
+}
+
